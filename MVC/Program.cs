d692ed252b5392d512cf0f09bf12a0cca49a328a
Kayra Kavlak <kayranium10@gmail.com>
2024-01-03 17:56:58 +0300

    nearly there

--- MVC/Program.cs
+++ MVC/Program.cs
@@ -1,9 +1,48 @@
+using DataAccess.Contexts;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+using MVC.Settings; 
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+#region IoC
+builder.Services.AddDbContext<Db>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+#endregion
+#region Auth
+builder.Services
+    .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    // We are adding authentication to the project using default Cookie authentication.
+
+    .AddCookie(config =>
+    // We configure the cookie to be created through the config action delegate; unlike func delegates, 
+    // action delegates do not return a result and they are generally used for configuration operations as seen here.
+
+    {
+        config.LoginPath = "/Account/Login";
+        // If an operation is attempted without logging into the system, redirect the user to the 
+        // Users controller -> Login action.
+
+        config.AccessDeniedPath = "/Account/AccessDenied";
+        // If an unauthorized operation is attempted after logging into the system, redirect the user to the 
+        // Users controller -> AccessDenied action.
+
+        // Way 1:
+        //config.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+        // Way 2: getting minute value from appsettings.json
+        config.ExpireTimeSpan = TimeSpan.FromMinutes(AppSettings.CookieExpirationInMinutes);
+        // Allow the cookie created after logging into the system to be valid for 30 minutes.
+
+        config.SlidingExpiration = true;
+        // When SlidingExpiration is set to true, the user's cookie expiration is extended by a specific duration 
+        // every time they perform an action in the system. If set to false, the user's cookie lifespan ends after 
+        // a specific duration after the initial login, requiring them to log in again.
+    });
+#endregion
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
---
+++ MVC/Settings/AppSettings.cs
@@ -0,0 +1,24 @@
+﻿namespace MVC.Settings
+{
+    /// <summary>
+    /// This class is used for binding JSON property values of the AppSettings section 
+    /// of the appsettings.json file. The section and class names must be the same,
+    /// also the property names must be the same for successful binding in the Program.cs file. 
+    /// </summary>
+    public class AppSettings
+    {
+        // static is used for directly reaching the value of the properties using the class name, 
+        // for example AppSettings.AppTitle or AppSettings.AppVersion without initialization.
+        // We can easily reach these properties in views and controller actions.
+        // static can be thought as a shared resource throughout the application, however
+        // only shared resources such as application configuration which will effect all the users
+        // of the application should be declared static, otherwise one change that a user performs
+        // may effect other users when they are using the application.
+
+        public static string AppTitle { get; set; }
+        public static double AppVersion { get; set; }
+
+        // for managing cookie expiration duration in appsettings.json
+        public static int CookieExpirationInMinutes { get; set; }
+    }
+}
